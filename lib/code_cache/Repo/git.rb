64aee1ea5256ca9c664180e38621fa539f354b14
David Buckhurst <david.buckhurst@bbc.co.uk>
2015-01-29 13:24:24 +0000

    Cache optimization

--- lib/code_cache/Repo/git.rb
+++ lib/code_cache/Repo/git.rb
@@ -3,8 +3,8 @@ require 'code_cache/repo'
 module CodeCache
   class Repo::Git < Repo
   
-    def initialize(url)
-      super(url)
+    def initialize(url, options = {})
+      super(url, options)
     end
     
   end
--- lib/code_cache/repo.rb
+++ lib/code_cache/repo.rb
@@ -1,13 +1,29 @@
+require 'fileutils'
+
 module CodeCache
 
   class Repo
     
-    attr_accessor :url
+    attr_accessor :url, :cache
     
-    def initialize(url)
+    def initialize(url, options = {})
+      @cache = options[:cache] || '/tmp/code_cache'
       @url = url
     end
+    
+    def create_cache(revision)
+      if !Dir.exist? location_in_cache(revision)
+        FileUtils.mkdir_p @cache
+      end
+    end
+    
+    def copy_from_cache
+      
+    end
   
+  end
+
+  class BadRepo < StandardError
   end
 
 end
