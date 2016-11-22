56f85631f9ca63e0c6cc6a7e2ffba03f8bd4f3af
asim <asimk21@gmail.com>
2016-11-22 11:07:00 +0000

    removed redundant code

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -1,33 +1,33 @@
 require 'code_cache/repo'
 require 'code_cache/repo/git'
-
-GIT_URL = 'git@github.com:bbc/code_cache.git'
-HTTPS_URL = 'https://github.com/bbc/code_cache.git'
+require 'git_helper'
 
 CACHE_BASE = '/tmp/code_cache_tests_'
 
 describe CodeCache::Repo::Git do
-  
+
+  let(:bare_repo){remote_git_repo('fake', {:branches => ['test']})}
+
   describe '.new' do
-    
+   
     it 'creates a repo object' do
-      repo = CodeCache::Repo::Git.new( GIT_URL )
+      repo = CodeCache::Repo::Git.new( bare_repo )
       expect(repo).to be_a CodeCache::Repo::Git
     end  
     
     it "throws an exception if the repo url can't be hit" do
-      expect{CodeCache::Repo::Git.new( GIT_URL + 'bad' )}.to raise_error(CodeCache::BadRepo)
+      expect{CodeCache::Repo::Git.new( bare_repo + 'bad' )}.to raise_error(CodeCache::BadRepo)
     end
       
   end
   
   describe '#clone_repo_to_cache' do
-    
-    it 'clones a repository to the cache without a checkout' do
+  let(:repo) { remote_git_repo('fake') }   
 
+ 
+    it 'clones a repository to the cache without a checkout' do
       cache = CACHE_BASE+rand(999999).to_s
-      
-      repo = CodeCache::Repo::Git.new( GIT_URL, :cache => cache )
+      repo = CodeCache::Repo::Git.new( bare_repo,  :cache => cache )
       
       cache_destination = repo.location_in_cache()
       repo.clone_bare_repo_to_cache(cache_destination)
@@ -44,7 +44,7 @@ describe CodeCache::Repo::Git do
       
       cache = CACHE_BASE+rand(999999).to_s
       
-      repo = CodeCache::Repo::Git.new( GIT_URL, :cache => cache )
+      repo = CodeCache::Repo::Git.new( bare_repo, :cache => cache )
       
       destination = "/tmp/checkouts/#{rand(999999)}/code_cache"
       
@@ -52,21 +52,33 @@ describe CodeCache::Repo::Git do
       
       expect( result ).to eq true
       
-      cached_clone = cache + '/git/github.com/bbc/code_cache/'
+      cached_clone = repo.location_in_cache()
       
       expect( Dir.exist?(cached_clone) ).to eq true
       expect( Dir.exist?(cached_clone + '/refs') ).to be true
       
       expect( Dir.exist?(destination) ).to eq true
       expect( Dir.exist?(destination+'/.git')).to eq true
-      expect( File.exist?(destination + '/README.md') ).to be true
-      
     end
     
+    it 'checks out the specific branch' do
+      cache = CACHE_BASE+rand(999999).to_s
+
+      repo = CodeCache::Repo::Git.new( bare_repo, :cache => cache)
+      
+      destination = "/tmp/checkouts/#{rand(999999)}/code_cache"
+      
+      result = repo.checkout( :head, destination, 'test' )
+      expect( Dir.exist?(destination) ).to eq true 
+      branch = `cd #{destination} && git branch`.strip.gsub!("* ","")
+      expect(branch).to eq 'test'
+    
+    end
+
     it 'checks out the same repo twice using the cache' do
 
       cache = CACHE_BASE+rand(999999).to_s
-      repo = CodeCache::Repo::Git.new( GIT_URL, :cache => cache )
+      repo = CodeCache::Repo::Git.new( bare_repo, :cache => cache )
       
       destination_1 = "/tmp/checkouts/#{rand(999999)}/code_cache"
       destination_2 = destination_1 + '-second'
@@ -95,7 +107,7 @@ describe CodeCache::Repo::Git do
       cache = CACHE_BASE+rand(999999).to_s
       `rm -rf #{cache}`
       
-      repo = CodeCache::Repo::Git.new( GIT_URL, :cache => cache )
+      repo = CodeCache::Repo::Git.new( bare_repo, :cache => cache )
       
       repo.create_cache(:head)
       
@@ -106,16 +118,11 @@ describe CodeCache::Repo::Git do
     
   describe '#location_in_cache' do
     
-    it 'calculates the location of a checkout in the cache from an ssh url' do
-      repo = CodeCache::Repo::Git.new( GIT_URL, :cache => '/tmp/cache' )
-      expect(repo.location_in_cache(:head)).to eq '/tmp/cache/git/github.com/bbc/code_cache/head'
+    it 'calculates the location of a checkout in the cache from an repo url' do
+      repo = CodeCache::Repo::Git.new( bare_repo, :cache => '/tmp/cache' )
+      expect(repo.location_in_cache(:head)).to eq '/tmp/cache/git/head'
     end
-    
-    it 'calculates the location of a checkout in the cache from an https url' do
-      repo = CodeCache::Repo::Git.new( HTTPS_URL, :cache => '/tmp/cache' )
-      expect(repo.location_in_cache(:head)).to eq '/tmp/cache/git/github.com/bbc/code_cache/head'
-    end
-    
+
   end
   
 end
